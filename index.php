d2232671f90f97600ca72f0446ad9318210c3bd0
Eugene Chanko <polarlarch@gmail.com>
2022-05-22 18:21:25 +0300

    add link

--- index.php
+++ index.php
@@ -9,6 +9,8 @@
     require 'blocks\nav.php';
      ?>
     <p>“Hello, <b>NIX Education</b>”</p>
+    <p><a href="https://github.com/EugeneChanko/nix_education_ls1.git">
+      Мой публичный репозиторий</a><p>
   <?php
     require 'blocks\footer.php'
   ?>
