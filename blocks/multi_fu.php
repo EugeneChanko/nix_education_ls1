1973b737be2db550dfb389f6f2626bc54202f59e
Eugene Chanko <polarlarch@gmail.com>
2022-05-29 17:31:55 +0300

    V 1.1 Lesson 1 and edits to the header and footer

---
+++ blocks/multi_fu.php
@@ -0,0 +1,7 @@
+<?php
+do {
+  $c=$a*$b;
+  echo "<br>$a*$b=$c";
+  $b++;
+} while ($b <= 10);
+?>
---
+++ les1.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html>
+  <body>
+    <link rel="stylesheet" href="ruls.css" type="text/css" >
+<?php
+require 'blocks\nav.php';
+?>
+<h6 class='block';>
+    <p class='try';>
+      <?php
+    $a=1;
+    $b=1;
+    require "blocks\multi_fu.php";
+    ?></p>
+
+    <p class='try';>  <?php
+    $a=2;
+    $b=1;
+    require 'blocks\multi_fu.php';
+    ?></p>
+
+    <p class='try';>  <?php
+    $a=3;
+    $b=1;
+    require 'blocks\multi_fu.php';
+      ?></p>
+
+    <p class='try';>  <?php
+    $a=4;
+    $b=1;
+    require 'blocks\multi_fu.php';
+      ?></p>
+
+    <p class='try';>  <?php
+    $a=5;
+    $b=1;
+    require 'blocks\multi_fu.php';
+      ?></p>
+
+  <br>
+
+
+    <p class='try';>
+      <?php
+    $a=6;
+    $b=1;
+    require "blocks\multi_fu.php";
+    ?></p>
+
+    <p class='try';>  <?php
+    $a=7;
+    $b=1;
+    require 'blocks\multi_fu.php';
+    ?></p>
+
+    <p class='try';>  <?php
+    $a=8;
+    $b=1;
+    require 'blocks\multi_fu.php';
+      ?></p>
+
+    <p class='try';>  <?php
+    $a=9;
+    $b=1;
+    require 'blocks\multi_fu.php';
+      ?></p>
+
+    <p class='try';>  <?php
+    $a=10;
+    $b=1;
+    require 'blocks\multi_fu.php';
+      ?></p>
+</h6>
+<?php
+require 'blocks\footer.php';
+  ?>
+  </p>
+  </body>
+</html>
